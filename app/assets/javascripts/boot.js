72a8a6f53f2a30f08ff0c19c9a579c4817c91fde
Dan Garland <dan@dangarland.co.uk>
2014-06-20 16:27:40 +0100

    Waiting for AJAX to finish before rendering the views

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	console.log(Backbone.history.start());
+	console.log(Backbone.history.start({ pushState: true }));
 	var router = new app.Router();
 	router.navigate("users", {trigger: true});
 });
