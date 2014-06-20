009e47700c84c98a11034ffe619ed75771532416
Dan Garland <dan@dangarland.co.uk>
2014-06-20 16:27:21 +0100

    Avoiding broken image_url

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -8,23 +8,33 @@ app.Router = Backbone.Router.extend({
 
 	userIndex: function() {
 		var users = new app.collections.UserList();
-	  users.fetch();
-	  if(users.length == 0) users.create({
-	  	name: "Edit me",
-	  	bio: "Edit me",
-	  	mission: "Edit me",
-	  	image_url: "uploads/me.jpg"
-	  });
+	  users.fetch({
+	  	success: function(users) {
+	  		// When rails gives us back some JSON data
+			  if(users.length == 0) users.create({
+			  	name: "Edit me",
+			  	bio: "Edit me",
+			  	mission: "Edit me",
+			  	image_url: "uploads/me.jpg"
+			  });
 
-	  var userIndexView = new app.views.UserListView({ collection : users });
-	  userIndexView.render();
+			  var userIndexView = new app.views.UserListView({ collection : users });
+			  userIndexView.render();
+	  	},
+	  	error: function() {
+	  		console.log("Couldn't save the user...");
+	  	}
+	  });
 	},
 
 	userShow: function(id) {
 		var user = new app.models.User({id: id});
-	  user.fetch();
+	  user.fetch({
+	  	success: function(user) {
+			  var userView = new app.views.UserView({ model: user });
+			  userView.render();
+	  	}
+	  });
 
-	  var userView = new app.views.UserView({ model: user });
-	  userView.render();
 	}
 });
