6981ff4def547259efd501105392c7da5e7568d4
Dan Garland <dan@dangarland.co.uk>
2014-06-23 14:19:45 +0100

    Skills belongs to Project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620140358) do
+ActiveRecord::Schema.define(version: 20140623131415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140620140358) do
     t.string   "url"
     t.text     "body"
     t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string   "name"
+    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
