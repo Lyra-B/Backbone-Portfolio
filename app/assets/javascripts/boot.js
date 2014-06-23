5ed681793a2492b8c6d49a8ab5f809bc5906a38d
Dan Garland <dan@dangarland.co.uk>
2014-06-23 17:24:03 +0100

    Hack to stop errors in test mode

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	console.log(Backbone.history.start({ pushState: true }));
-	var router = new app.Router();
-	router.navigate("users", {trigger: true});
+	// Little hack to stop running the app in test mode
+	if(!window.jasmine) {
+		console.log(Backbone.history.start({ pushState: true }));
+		var router = new app.Router();
+		router.navigate("users", {trigger: true});
+	}
 });
