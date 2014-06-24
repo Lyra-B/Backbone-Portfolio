c4b7bb51ff56f0f27baf800d5b3588596a329288
Dan Garland <dan@dangarland.co.uk>
2014-06-24 18:45:57 +0100

    Arguably making this less ugly

--- app/assets/javascripts/views/user_view.js
+++ app/assets/javascripts/views/user_view.js
@@ -9,38 +9,32 @@ app.views.UserView = Backbone.View.extend({
 
 	initialize: function() {
 		this.listenTo(this.model.projects, "reset", this.render);
+		this.listenTo(this.model, "change", this.render);
 	},
 
 	render: function() {
 		// Render the user bio section
 		var _this = this;
 		$('#user-list').slideUp(400, function() {
-			_this.$el.html(_this.template(_this.model.attributes)).hide();
-
-			// Create a dummy project if there isn't one already
-		  if(_this.model.projects.length == 0) {
-		    // Create a blank project for us to fill in.
-		    _this.model.projects.add({
-		      title: "New Project",
-		      url: "Click to edit",
-		      body: "Click to edit"
-		    });
-		  }
-		  
-		  // Render each project... sort of like a partial...
-		  _this.model.projects.each(function(project) {
-		    var view = new app.views.ProjectView({ model: project });
-		    $('#project-list').append(view.render().el);
-		  });
-
-		  // Redraw the page if the user model changes
-			_this.listenTo(_this.model, "change", _this.render);
-
-			_this.$el.slideDown();
-			
-			return _this;
+			_this.showPage();
 		});
-		
+
+		return this;
+	},
+
+	showPage: function() {
+		this.$el.html(this.template(this.model.attributes)).hide();
+
+		// Create a dummy project if there isn't one already
+	  this.createDefaultProject();
+	  
+	  // Render each project... sort of like a partial...
+	  this.model.projects.each(function(project) {
+	    var view = new app.views.ProjectView({ model: project });
+	    $('#project-list').append(view.render().el);
+	  });
+
+		this.$el.slideDown();
 	},
 
 	editName: function(e) {
@@ -50,6 +44,17 @@ app.views.UserView = Backbone.View.extend({
 	updateName: function(e) {
 		this.model.set('name', e.currentTarget.value);
 		this.model.save();
+	},
+
+	createDefaultProject: function() {
+    // Create a blank project for us to fill in.
+		if(this.model.projects.length == 0) {
+	    this.model.projects.add({
+	      title: "New Project",
+	      url: "Click to edit",
+	      body: "Click to edit"
+	    });
+	  }
 	}
 
 });
