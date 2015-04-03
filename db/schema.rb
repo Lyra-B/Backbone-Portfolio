6ec2bf368efe2bf2ce542962b3d20e5f55541b96
Lyra-B <glykeriapeppa@gmail.com>
2015-04-03 10:09:15 +0100

    Added body to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402125744) do
+ActiveRecord::Schema.define(version: 20150403090617) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150402125744) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
   create_table "users", force: :cascade do |t|
