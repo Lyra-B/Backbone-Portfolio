1c7a54e6840f7399f893c3e538363a2a64876924
Lyra-B <glykeriapeppa@gmail.com>
2015-04-04 18:07:17 +0100

    Created models skill and projectSkill

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403103845) do
+ActiveRecord::Schema.define(version: 20150404170444) do
+
+  create_table "project_skills", force: :cascade do |t|
+    t.integer  "project_id"
+    t.integer  "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +28,12 @@ ActiveRecord::Schema.define(version: 20150403103845) do
     t.datetime "updated_at", null: false
     t.text     "body"
     t.string   "image_url"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
