a3375e53f499023bb59f790888154b60d4d56a23
Lyra-B <glykeriapeppa@gmail.com>
2015-04-08 11:30:45 +0100

    Added as_json methid

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,13 @@ class Project < ActiveRecord::Base
   # has_many :project_skills
   has_many :skills  #, through: :project_skills
 
-  accepts_nested_attributes_for :skills
+  accepts_nested_attributes_for :skills, :allow_destroy => true
+
+  def as_json(options = nil)
+    json_options = { :except => [:created_at, :updated_at], :include => {
+      :skills => { :only => [:id, :name] }
+    }}.merge(options)
+    super(json_options)
+  end
+
 end
