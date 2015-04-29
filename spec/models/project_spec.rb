d70a47f6f20d8fe99f1a22e478275e8a23b200c3
Lyra-B <glykeriapeppa@gmail.com>
2015-04-29 16:02:04 +0100

    Stubbed test for instagram

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Fetching images from Instagram" do
+    before do
+      stub_request(:get, "https://api.instagram.com/v1/tags/Pretty+Flowers/media/recent").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer 12345', 'User-Agent'=>'Faraday v0.9.1'}).
+         to_return(:status => 200, :body => File.open('spec/fixtures/cat.json').read, :headers => {})
+
+      @user = User.create!(:instagram_access_token => "12345")
+      @project = @user.projects.create!(:title => "Pretty Flowers")
+    end
+
+    it "should automatically find an image from Instagram" do
+      expect(@project.reload.image_url).to_not be nil
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'webmock/rspec'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
