5a07f3f4c8cefee020bf546b55c480099b0f0b94
Lyra-B <glykeriapeppa@gmail.com>
2015-04-29 15:58:52 +0100

    Added oauth access token method

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,4 +12,12 @@ class Project < ActiveRecord::Base
     super(json_options)
   end
 
+  before_save do
+    token = OAuth2::AccessToken.new(OAuth2::Client.new(
+      Rails.application.secrets.instagram["client_id"],
+      Rails.application.secrets.instagram["client_secret"],
+      :site => "https://api.instagram.com"), self.user.instagram_access_token)
+    results = JSON(token.get("/v1/tags/#{CGI.escape title}/media/recent?access_token=#{self.user.instagram_access_token}"))
+    self.image_url = results["data"].first["images"]["standard_resolution"]["url"]
+  end
 end
